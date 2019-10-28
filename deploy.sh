f2007baa660b2fb86119c1315fa25d125cbcafa2
Emanuel Lazaro <emanuelllazaro@gmail.com>
2019-10-27 23:41:13 -0300

    Fixing deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,4 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 echo "Deploying to $env"
 echo "_______________________________"
 export SLS_DEBUG=*
-serverless deploy --stage $env -r us-east-1 --package /artifacts/$env -v 
+serverless deploy --stage $env -r us-east-1 --package $CODEBUILD_SRC_DIR/artifacts/$env -v 
