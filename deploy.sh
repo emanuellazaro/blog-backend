ea748773836cb2bab25a27728bc91aba369cbd13
Emanuel Lazaro <emanuelllazaro@gmail.com>
2019-10-27 23:48:02 -0300

    Adding a new step to copy to .serverless directory on deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,6 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 echo "Deploying to $env"
 echo "_______________________________"
 export SLS_DEBUG=*
-serverless deploy --stage $env -r us-east-1 --package $CODEBUILD_SRC_DIR/artifacts/$env -v 
+mkdir -p .serverless
+cp -r $CODEBUILD_SRC_DIR/target/$env/* $PWD/.serverless
+serverless deploy --stage $env -r us-east-1 --package /.serverless -v 
